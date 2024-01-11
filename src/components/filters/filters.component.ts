6c8f55c5e737d56d0e08aaaddfc83796c8369306
Marcin <marcinkrueger@gmail.com>
2024-01-11 01:48:55 +0100

    add filters to match page

--- src/components/filters/filters.component.ts
+++ src/components/filters/filters.component.ts
@@ -1,12 +1,71 @@
 import { Component } from '@angular/core';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSliderModule } from '@angular/material/slider';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
   selector: 'app-filters',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCheckboxModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatProgressBarModule,
+    MatSelectModule,
+    MatSliderModule,
+    ReactiveFormsModule,
+    MatDividerModule,
+  ],
   templateUrl: './filters.component.html',
-  styleUrl: './filters.component.css'
+  styleUrl: './filters.component.css',
 })
 export class FiltersComponent {
+  formatKm(value: number) {
+    return value + 'km';
+  }
 
+  filtersForm = new FormGroup({
+    nameInput: new FormControl(''),
+    distanceInput: new FormControl(20),
+    minAgeInput: new FormControl(50),
+    maxAgeInput: new FormControl(110),
+    hobbyInput: new FormControl(''),
+    languageInput: new FormControl(''),
+  });
+  
+  onClick(e: Event) {
+    e.preventDefault();
+    console.log(this.filtersForm.value)
+  }
+
+  hobbyVariants: string[] = [
+    'Football',
+    'Tennis',
+    'Cooking',
+    'Astronomy',
+    'Games',
+  ];
+
+  languageVariants: string[] = [
+    'English',
+    'Spanish',
+    'German',
+    'French',
+    'Russian',
+  ];
+  // hideRequiredControl = new FormControl(false);
+  // floatLabelControl = new FormControl('auto' as FloatLabelType);
 }
--- src/views/search-page/search-page.component.ts
+++ src/views/search-page/search-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { PageComponent } from '../../components/page/page.component';
+import { FiltersComponent } from '../../components/filters/filters.component';
 
 @Component({
   selector: 'app-search-page',
   standalone: true,
-  imports: [PageComponent],
+  imports: [PageComponent, FiltersComponent],
   templateUrl: './search-page.component.html',
   styleUrl: './search-page.component.css'
 })
