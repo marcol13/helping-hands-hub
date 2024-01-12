14b9363992c806003c75207c9c9b160f3821551f
Marcin <marcinkrueger@gmail.com>
2024-01-12 02:10:45 +0100

    add about page

--- src/views/about/about.component.ts
+++ src/views/about/about.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { PageComponent } from '../../components/page/page.component';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [PageComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
